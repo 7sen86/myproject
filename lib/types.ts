c00b9ccf120aa1c76f967883f3a8034a925054ea
7sen86 <h941566@gmail.com>
2026-07-27 01:48:49 +0300

    Delete lib directory

--- lib/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Booklet, Subject, Stage, Teacher } from "@prisma/client";
-
-export type BookletWithRelations = Booklet & {
-  subject: Subject;
-  stage: Stage;
-  teacher: Teacher;
-};
